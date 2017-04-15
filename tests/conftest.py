62623a50d5e841ee1d5f1514a20b7243250c2972
Vince Reuter <vince.reuter@gmail.com>
2017-04-14 22:16:57 -0400

    just provide an identity processor across cores count grid

--- tests/conftest.py
+++ tests/conftest.py
@@ -54,13 +54,30 @@ def unaligned_reads_file():
 
 
 
-@pytest.fixture(scope="function")
-def identity_processor(request):
+@pytest.fixture(scope="function", params=[1, 2, 4])
+def num_cores_opt(request):
+    """ For quick tests, validate across several cores count values. """
+    return request.param
 
-    if CORES_PARAM_NAME not in request.fixturenames:
-        raise Exception("To create a processor, test case must "
-                        "specify '{}' as a fixture".format(CORES_PARAM_NAME))
-    num_cores = request.getfixturevalue(CORES_PARAM_NAME)
+
+
+@pytest.fixture(scope="function")
+def identity_processor(request, num_cores_opt):
+    """
+    Provide a basic processor for a fast test of some behavior.
+    
+    Parameters
+    ----------
+    request : pytest.fixtures.SubRequest
+        Test case requesting the fixture parameterization.
+
+    Returns
+    -------
+    pararead.ParaReadProcessor
+        A very basic processor, returning elements with no 
+        or very trivial modification(s) for speed.
+
+    """
 
     if IS_ALIGNED_PARAM_NAME in request.fixturenames \
             and not request.getfixturevalue(IS_ALIGNED_PARAM_NAME):
@@ -68,4 +85,4 @@ def identity_processor(request):
     else:
         path_reads_file = PATH_ALIGNED_FILE
 
-    return IdentityProcessor(path_reads_file, cores=num_cores)
+    return IdentityProcessor(path_reads_file, cores=num_cores_opt)
